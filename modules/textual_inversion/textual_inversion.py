b8b599048908ae7518ca6eea0f8911f77b4c12e9
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-10-13 23:12:50 +0700

    Update textual_inversion.py

--- modules/textual_inversion/textual_inversion.py
+++ modules/textual_inversion/textual_inversion.py
@@ -579,7 +579,7 @@ def train_embedding(id_task, embedding_name, learn_rate, batch_size, gradient_st
 
                     shared.sd_model.first_stage_model.to(devices.device)
 
-                    p = processing.StableDiffusionProcessingTxt2Img(
+                    p = processing.SdwProcessingTxt2Img(
                         sd_model=shared.sd_model,
                         do_not_save_grid=True,
                         do_not_save_samples=True,
