853ad805b75471cf4fb7bd1b7cddb25edf48b29d
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-10-13 22:17:36 +0700

    Update ui.py

--- modules/ui.py
+++ modules/ui.py
@@ -1264,7 +1264,7 @@ def create_ui():
     for _interface, label, _ifid in interfaces:
         shared.tab_names.append(label)
 
-    with gr.Blocks(theme=shared.gradio_theme, analytics_enabled=False, title="Stable Diffusion") as demo:
+    with gr.Blocks(theme=shared.gradio_theme, analytics_enabled=False, title="sdw") as demo:
         settings.add_quicksettings()
 
         parameters_copypaste.connect_paste_params_buttons()
