8d636bff834fed462d75a7c925cfd40336291f3c
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-10-13 22:35:12 +0700

    Update sd_models.py

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -499,9 +499,9 @@ class SdModelData:
                     load_model()
 
                 except Exception as e:
-                    errors.display(e, "loading stable diffusion model", full_traceback=True)
+                    errors.display(e, "loading Sdw model", full_traceback=True)
                     print("", file=sys.stderr)
-                    print("Stable diffusion model failed to load", file=sys.stderr)
+                    print("Sdw model failed to load", file=sys.stderr)
                     self.sd_model = None
 
         return self.sd_model
